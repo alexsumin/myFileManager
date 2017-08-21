eb2528b6a03d5445f4e411cd9f3fc4a2756d326f
aleksandr-sumin <sumin.alexandr@gmail.com>
2017-08-22 02:15:02 +0300

    add icon to application

--- src/main/java/ru/alexsumin/filemanager/Main.java
+++ src/main/java/ru/alexsumin/filemanager/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/view/MainWindow.fxml"));
         primaryStage.setTitle("File Manager");
         primaryStage.setScene(new Scene(root, 640, 600));
+        Image ico = new Image(getClass().getResourceAsStream("/images/app.png"));
+        primaryStage.getIcons().add(ico);
         primaryStage.show();
     }
 }
