e7ded1fdf43058f3ff34278002835d0ebabb0acb
aleksandr-sumin <sumin.alexandr@gmail.com>
2017-08-28 02:43:08 +0300

    little fix

--- src/main/java/ru/alexsumin/filemanager/model/MyTreeCell.java
+++ src/main/java/ru/alexsumin/filemanager/model/MyTreeCell.java
@@ -8,7 +8,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import ru.alexsumin.filemanager.view.MainWindowController;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
@@ -61,7 +60,6 @@ public class MyTreeCell extends TreeCell<Path> {
                         this.setText(null);
                         this.setGraphic(null);
                     } else {
-                        //setTooltipForFile(this);
                         this.setPrefHeight(40);
                         this.setText(getString());
                     }
@@ -113,98 +111,11 @@ public class MyTreeCell extends TreeCell<Path> {
         }
     }
 
-//    void setImageForNode() {
-//
-//
-//        if (this.getTreeItem().equals(MainWindowController.root)) {
-//            this.setGraphic(new ImageView(picPc));
-//            return;
-//        }
-//
-//
-//        this.getTreeItem().setGraphic(new ImageView(picLoading));
-//
-//
-//        Task<ImageView> graphicTask = new Task<ImageView>() {
-//
-//            @Override
-//            protected ImageView call() {
-//                System.out.println("таска для " + getTreeItem());
-//
-//                ImageView imageView = new ImageView(picFile);
-//                Path path = getTreeItem().getValue();
-//
-//
-//                    String s = null;
-//                    try {
-//                        s = Files.probeContentType(path);
-//                        if (s.startsWith("image")) {
-//                            if (Files.size(path) < 5 * 1024 * 1024) {
-//                                Image image = new Image("file:" + path);
-//
-//                                if (image.isError()) {
-//                                    return imageView = new ImageView(picImage);
-//                                } else {
-//                                    imageView = new ImageView(image);
-//                                    imageView.setFitHeight(40);
-//                                    imageView.setFitWidth(40);
-//                                    imageView.setPreserveRatio(true);
-//                                    return imageView;
-//                                }
-//                            } else {
-//                                return imageView = new ImageView(picImage);
-//                            }
-//                        }
-//                    } catch (IOException e) {
-//                        return new ImageView(picFile);
-//                    }
-//                return null;
-//                }
-//
-//
-//
-//
-//        };
-//
-////        graphicTask.setOnRunning(event -> {
-////            ImageView imageView = graphicTask.getValue();
-////            setGraphic(imageView);
-////        });
-//
-//        graphicTask.setOnSucceeded(event -> {
-//            ImageView imageView = graphicTask.getValue();
-//            setGraphic(imageView);
-//        });
-//
-//        MainWindowController.EXEC.submit(graphicTask);
-//    }
-
     @Override
     public void startEdit() {
 
     }
 
-
-//    private void setTooltipForFile(MyTreeCell cell) {
-//        File file = getItem();
-//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-//
-//        if (getItem().isDirectory()) {
-//            cell.setTooltip(new Tooltip("Type: \t" + "folder" +
-//                    "\nFiles: \t" + numberFiles(file) + "\n" + "Modified: \t" + sdf.format(file.lastModified())));
-//        } else {
-//            cell.setTooltip(new Tooltip("Type: \t" + "file" +
-//                    "\n" + "Modified: \t" + sdf.format(file.lastModified())));
-//        }
-//    }
-
-    private String numberFiles(File file) {
-        try {
-            return String.valueOf(file.listFiles().length);
-        } catch (Exception e) {
-            return "unavailable";
-        }
-    }
 
     private String getString() {
         return ((TreeItemWithLoading) getTreeItem()).toString();
